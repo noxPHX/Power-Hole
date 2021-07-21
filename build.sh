4899e2b544ebbc6714ac81dee1c76c5e52e89bc8
Manu <emmanuel.feral@protonmail.com>
2021-07-21 15:10:19 +0200

    Working recursor

--- build.sh
+++ build.sh
@@ -19,4 +19,5 @@ cd "$this_script_path" || exit 1
 
 # Compose does not allow yet BuildKit secrets
 export DOCKER_BUILDKIT=1
-docker build --secret id=db_password,src=secrets/db_password.txt --secret id=api_key,src=secrets/api_key.txt -t powerdns:server .
+docker build --secret id=db_password,src=secrets/db_password.txt --secret id=api_key,src=secrets/api_key.txt -t powerdns:authoritative authoritative
+docker build -t powerdns:recursor recursor
